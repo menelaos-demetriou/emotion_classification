7233dfa79959c67545c1a1f35396ae25ef548166
Menelaos Dimitriou <menelaos.dimitrio@gmail.com>
2020-08-18 15:32:47 +0100

    Fixed dvc

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 import PIL
-import PIL.Image
+import PIL.Image as img
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def pie_plot(data):
-    labels = 'happiness', 'disgust', 'neutral', 'sadness', 'anger', 'surprise', 'fear'
+    labels = 'neutral', 'happiness', 'surprise', 'sadness', 'anger', 'disgust', 'fear', 'contempt'
 
     fig1, ax1 = plt.subplots()
     ax1.pie(data.value_counts(normalize=True), labels=labels, autopct='%1.1f%%',
@@ -22,15 +22,29 @@ def pie_plot(data):
 def main():
     csv1 = pd.read_csv("data/legend.csv")
     csv1 = csv1.drop("user.id", axis=1)
-    csv1 = csv1.set_index("image")
-
     map_dict = {"HAPPINESS": "happiness", "DISGUST": "disgust", "NEUTRAL": "neutral",
                 "SADNESS": "sadness", "FEAR": "fear", "SURPRISE": "surprise", "ANGER": "anger"}
 
-    csv1["emotion"] = csv1["emotion"].map(map_dict)
+    csv1["emotion"] = csv1["emotion"].replace(map_dict)
     print(csv1["emotion"].value_counts(normalize=True))
 
-    pie_plot(csv1["emotion"])
+    # pie_plot(csv1["emotion"])
+
+    # Add dir into image name
+    csv1["image"] = "data/images/" + csv1["image"]
+
+    # Shuffle dataset
+    csv1.sample(n=1, random_state=1)
+
+    image = img.open(csv1["image"][0])
+    plt.imshow(image)
+    plt.show()
+
+    # Convert all images in the same size and greyscale
+
+    # Plot each target example image
+
+    # Tensorflow csv dataset with batches
 
 
 if __name__ == "__main__":
